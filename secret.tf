11f4002713830b7e2dd6564394e02ad2efce91e5
Adarsh Hiwrale <ahiwrale@sigmasolve.net>
2023-05-10 20:17:15 +0530

    removed event statement

--- secret.tf
+++ secret.tf
@@ -68,18 +68,6 @@ data "aws_iam_policy_document" "kms_key_access_policy_doc" {
     }
   }
 
-  statement {
-    effect    = "Allow"
-    actions   = ["kms:Decrypt","kms:GenerateDataKey"]
-    resources = ["*"]
-    sid = "AllowEventRule"
-
-    principals {
-      type        = "Service"
-      identifiers = ["events.amazonaws.com"]
-    }
-  }
-
   dynamic "statement" {
     for_each = length(local.secret_read_principals) == 0 ? [] : [1]
     content {
