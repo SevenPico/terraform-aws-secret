d299106a6f59c3660f0922d14a8d352fa34bf044
Adarsh Hiwrale <ahiwrale@sigmasolve.net>
2023-09-06 02:43:08 +0530

    condition changed

--- secret.tf
+++ secret.tf
@@ -32,7 +32,7 @@ locals {
 }
 
 data "aws_kms_key" "kms_key" {
-  count = module.context.enabled && var.kms_key_enabled ? 1 : 0
+  count = module.context.enabled && var.kms_key_enabled ? 0 : 1
   key_id = var.kms_key_id
 }
 
