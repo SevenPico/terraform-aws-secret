39507972e118dcd01738c42be45626547584d2f9
Adarsh Hiwrale <ahiwrale@sigmasolve.net>
2023-09-05 22:01:15 +0530

    fixing count

--- _outputs.tf
+++ _outputs.tf
@@ -28,19 +28,19 @@ output "id" {
 }
 
 output "kms_key_arn" {
-  value = module.kms_key[0].key_arn
+  value = try(module.kms_key[0].key_arn, "")
 }
 
 output "kms_key_id" {
-  value = module.kms_key[0].key_id
+  value = try(module.kms_key[0].key_id, "")
 }
 
 output "kms_key_alias_name" {
-  value = module.kms_key[0].alias_name
+  value = try(module.kms_key[0].alias_name, "")
 }
 
 output "kms_key_alias_arn" {
-  value = module.kms_key[0].alias_arn
+  value = try(module.kms_key[0].alias_arn, "")
 }
 
 output "sns_topic_arn" {
