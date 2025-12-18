ea062870ae39fb2226e99edd2b25e1c007af2640
Dave Raring <d@7pi.co>
2025-12-17 18:15:41 -0600

    trying to fix Count Issues

--- secret.tf
+++ secret.tf
@@ -33,7 +33,7 @@ locals {
 }
 
 data "aws_kms_key" "kms_key" {
-  count  = module.context.enabled && !local.kms_key_enabled ? 1 : 0
+  count  = var.kms_key_id != null && var.kms_key_id != "" ? 1 : 0
   key_id = var.kms_key_id
 }
 
